78a07250a48bcd18da189bdc59d526d0fc9cd965
Дмитрий <grunger.ekb@gmail.com>
2020-08-20 20:00:27 +0500

    first commit

---
+++ src/pages/Example.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+        <section>
+            <div class="container">
+                <h1>Example page</h1>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
